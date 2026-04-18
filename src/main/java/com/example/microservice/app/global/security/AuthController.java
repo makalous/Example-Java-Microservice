0719edc792c1909b3cdb365b17ee92e4740d3741
makser <mak.ajwen@gmail.com>
2026-04-18 14:05:36 +0200

    minor auth changes

--- src/main/java/com/example/microservice/app/global/security/AuthController.java
+++ src/main/java/com/example/microservice/app/global/security/AuthController.java
@@ -2,7 +2,10 @@ package com.example.microservice.app.global.security;
 
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -10,10 +13,9 @@ import org.springframework.web.bind.annotation.*;
 @Hidden
 public class AuthController {
     private final JwtTokenService tokenService;
-
     @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password) {
+    public ResponseEntity<Map<String, String>> login(@RequestParam String username, @RequestParam String password) {
         //TODO e.g. validation with password - depends on needs
-        return "Bearer " + tokenService.generateToken(username);
+        return ResponseEntity.ok(Map.of("Bearer", tokenService.generateToken(username)));
     }
 }
